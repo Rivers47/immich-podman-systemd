3a119b8aa5c1ce2a0f614436516ce634b819e68e
Rivers Chen <riverscbc47@gmail.com>
2025-09-16 02:10:16 -0400

    Update to v1.142.0

--- migrate.sh
+++ migrate.sh
@@ -13,6 +13,6 @@ REDIS_IMAGE_TAG=$(yq e '.services.redis.image' docker-compose.yml)
 sed -i "s|^Image=.*$|Image=${REDIS_IMAGE_TAG}|" immich-redis.container
 
 
-sed -i "s/v[0-9]\.[0-9]\{3\}\.[0-9]/${NEW_VERSION}/" *.container *.image
+sed -i "s/v[0-9]\.[0-9]\{3\}\.[0-9]/${NEW_VERSION}/" *.container *.image alternative/*.container
 
 rm docker-compose.yml
